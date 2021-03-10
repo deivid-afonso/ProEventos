e53962d533e79b83d50c6c247c86e31e4d5b8b46
deivid-afonso <deividsa@gmail.com>
2021-03-10 10:06:42 -0300

    Seção 3 intro ef core

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -12,66 +13,43 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+      
+        private readonly DataContext _context;
+
+        public EventoController(DataContext context)
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+            this._context = context;
 
-       public IEnumerable<Evento> _evento =  new Evento[]{
-             new Evento(){
-                    EventoId = 1,
-                    Tema = "Angular 11 e .net5", 
-                    Local = "BH",
-                    Lote = "1º lote", 
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString(),
-                    ImagemUrl = "foto.png"
-                    },
-
-              new Evento(){
-                    EventoId = 2,
-                    Tema = "Angular 11 e ", 
-                    Local = "sp",
-                    Lote = "2º lote", 
-                    QtdPessoas = 300,
-                    DataEvento = DateTime.Now.AddDays(3).ToString(),
-                    ImagemUrl = "foto1.png"
-                    }
-          };
-
-        public EventoController()
-        {
-           
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-          return _evento;
+            return _context.Eventos;
         }
 
-         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        [HttpGet("{id}")]
+        public Evento GetById(int id)
         {
-          return _evento.Where(evento => evento.EventoId == id);
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
         }
 
-         [HttpPost]
+        [HttpPost]
         public string Post()
         {
-          return "post example";
+            return "post example";
         }
 
-         [HttpPut("{id}")]
+        [HttpPut("{id}")]
         public string Put(int id)
         {
-          return $"put example with id = {id}";
+            return $"put example with id = {id}";
         }
 
-         [HttpDelete("{id}")]
+        [HttpDelete("{id}")]
         public string Delete(int id)
         {
-          return $"delete example with id = {id}";
+            return $"delete example with id = {id}";
         }
     }
 }
